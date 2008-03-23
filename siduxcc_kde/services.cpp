62ad78d345ebac1dd7bfa12ef5103e0910e38c4b
Fabian Wuertz <xadras@users.berlios.de>
2008-03-23 23:06:42 +0000

    * kde: services: descriptions updated

--- siduxcc_kde/services.cpp
+++ siduxcc_kde/services.cpp
@@ -102,7 +102,6 @@ void services::back()
 
 void services::save()
 {
-
 	// update Service
 	QString service = servicesBox->currentText();
 
@@ -134,7 +133,6 @@ void services::save()
 	updateSlot();
 
 	applyPushButton->setEnabled(FALSE);
-
 }
 
 
@@ -180,8 +178,6 @@ void services::runServiceAction(QString action)
 
 void services::updateSlot()
 {
-
-
 	QString service = servicesBox->currentText();
 	int i = servicesBox->currentItem();
 
@@ -254,7 +250,8 @@ void services::disableService()
 }
 
 
-void services::enableService(){
+void services::enableService()
+{
 	QString currentService = disabledServices->currentText();
 	if(!currentService) return;
 	QValueList<QString>::Iterator it = this->servicesToDisable.find(currentService);
@@ -275,46 +272,53 @@ void services::showDescription(QListBoxItem* input)
 		 text = i18n("Advanced Configuration and Power Interface (ACPI) to allow intelligent power management on your system.");
 	else if( input->text() == "apache2" )
 		 text = i18n("Apache v2 is the next generation of the omnipresent Apache web server.");
+	else if( input->text() == "bluetooth" )
+		text = i18n("Enables Universal interface for wireless communication");
 	else if( input->text() == "cron" )
 		text = i18n("Cron is a time-based scheduling service.");
+	else if( input->text() == "cupsys" )
+		text = i18n("CUPS (Common UNIX Printing System) is a printing system. It supports the IPP (Internet Printing Protocol), and has its own filtering driver model for handling various document types.");
 	else if( input->text() == "gpm" )
 		text = i18n("gpm means general purpose mouse and is the mouse support for linux in the console.");
 	else if( input->text() == "hal" )
 		text = i18n("HAL provides an abstract view on hardware. This abstraction layer is simply an interface that makes it possible to add support for new devices and new ways of connecting devices to the computer, without modifying every application that uses the device.");
 	else if( input->text() == "ifplugd" )
 		text = i18n("Ifplugd is a daemon which will automatically configure your ethernet device when a cable is plugged in and automatically unconfigure it if the cable is pulled. This is useful on laptops with onboard network adapters, since it will only configure the interface when a cable is really connected.");
-	else if( input->text() == "IrDA" )
+	else if( input->text() == "irda-utils" )
 		text = i18n("IrDA (Infrared Data Association) is an industry standard for infrared wireless communication. Most laptops sold today are equipped with an IrDA compliant infrared transceiver, which enables you to communicate with devices such as printers, modems, fax, LAN, and other laptops.");
-	else if( input->text() == "klogd" )
-		text = i18n("klogd is a system daemon which intercepts and logs Linux kernel messages.");
-	else if( input->text() == "rsync" )
-		text = i18n("Rsync is a command line utility traditionally used in synchronizing files between two computers, but rsync can also be used as an effective backup tool. In daemon mode, rsync listens to the default TCP port of 873, serving files in the native rsync protocol. rsync can also be used to synchronize local directories, or via a remote shell such as RSH or SSH.");
-	else if( input->text() == "ssh" )
-		text = i18n("SSH is the portable version of OpenSSH, a free implementation of the Secure Shell protocol as specified by the IETF secsh working group.");
-	else if( input->text() == "sysklog" )
-		text = i18n("Sysklogd provides two system utilities which provide support for system logging and kernel message trapping. Support of both internet and unix domain sockets enables this utility package to support both local and remote logging.");
-	else if( input->text() == "vdr" )
-		text = i18n("Linux Video Disk Recorder (vdr)");
-	else if( input->text() == "vdradmin" )
-		text = i18n("vdradmin-am provides a webinterface for managing the Linux Video Disk Recorder (vdr)");
-	else if( input->text() == "bluetooth" )
-		text = i18n("Enables Universal interface for wireless communication");
-	else if( input->text() == "cupsys" )
-		text = i18n("CUPS (Common UNIX Printing System) is a printing system. It supports the IPP (Internet Printing Protocol), and has its own filtering driver model for handling various document types.");
 	else if( input->text() == "irqbalance" )
 		text = i18n("Irqbalance is a Linux daemon that distributes interrupts over the processors and cores you have in your computer system. The design goal of irqbalance is to do find a balance between power savings and optimal performance.");
 	else if( input->text() == "isdnutils" )
 		text = i18n("Isndutils is only for use with an internal ISDN adapter.");
+	else if( input->text() == "klogd" )
+		text = i18n("klogd is a system daemon which intercepts and logs Linux kernel messages.");
 	else if( input->text() == "lirc" )
 		text = i18n("LIRC is a package that allows you to decode and send infra-red signals of many (but not all) commonly used remote controls.");
-	else if( input->text() == "mysql" )
+	else if( input->text().contains("mysql") )
 		text = i18n("MySQL is a fast, stable and true multi-user, multi-threaded SQL database server.");
+	else if( input->text().contains("nvidia") )
+		text = i18n("These binary drivers provide optimized hardware acceleration of  OpenGL applications via a direct-rendering X Server.");
+	else if( input->text() == "powersaved" )
+		text = i18n("Support for ACPI, APM and CPU frequency scaling.");
+	else if( input->text() == "rsync" )
+		text = i18n("Rsync is a command line utility traditionally used in synchronizing files between two computers, but rsync can also be used as an effective backup tool. In daemon mode, rsync listens to the default TCP port of 873, serving files in the native rsync protocol. rsync can also be used to synchronize local directories, or via a remote shell such as RSH or SSH.");
+	else if( input->text() == "sqlrelay" )
+		text = i18n("SQL Relay is a persistent database connection pooling, proxying and load balancing system.");
+	else if( input->text() == "ssh" )
+		text = i18n("SSH is the portable version of OpenSSH, a free implementation of the Secure Shell protocol as specified by the IETF secsh working group.");
+	else if( input->text() == "sysklogd" )
+		text = i18n("Sysklogd provides two system utilities which provide support for system logging and kernel message trapping. Support of both internet and unix domain sockets enables this utility package to support both local and remote logging.");
+	else if( input->text() == "timidity" )
+		text = i18n("TiMidity++ is a very high quality software-only MIDI sequencer.");
+	else if( input->text() == "vdr" )
+		text = i18n("Linux Video Disk Recorder (vdr)");
+	else if( input->text() == "vdradmin" )
+		text = i18n("vdradmin-am provides a webinterface for managing the Linux Video Disk Recorder (vdr)");
 	else if( input->text() == "virtualbox-ose" )
 		text = i18n("VirtualBox is an application installed on an existing host operating system; within this application, additional operating systems can be loaded and run, each with its own virtual environment.");
 
 
 	descriptionLabel2->setText( text );
-
 }
 
 
