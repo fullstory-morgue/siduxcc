61d080a158505a9df097524b76eea914dbd2ba18
Fabian Wuertz <xadras@users.berlios.de>
2008-03-23 22:21:09 +0000

    * kde: services: descriptions updated

--- siduxcc_kde/services.cpp
+++ siduxcc_kde/services.cpp
@@ -67,7 +67,7 @@ void services::load()
 	// description
 
 	description.append( i18n("Apache v2 is the next generation of the omnipresent Apache web server.") );
-	description.append( i18n("The Common UNIX Printing System (or CUPS(tm)) is a printing system and general replacement for lpd and the like.") );
+	description.append( i18n("CUPS (Common UNIX Printing System) is a printing system. It supports the IPP (Internet Printing Protocol), and has its own filtering driver model for handling various document types.") );
 	description.append( i18n("MySQL is a fast, stable and true multi-user, multi-threaded SQL database server.") );
 	description.append( i18n("The Samba software suite is a collection of programs that implements the SMB/CIFS protocol for unix systems, allowing you to serve files and printers to Windows, NT, OS/2 and DOS clients.") );
 	description.append( i18n("SSH is the portable version of OpenSSH, a free implementation of the Secure Shell protocol as specified by the IETF secsh working group.") );
@@ -273,6 +273,8 @@ void services::showDescription(QListBoxItem* input)
 	QString text;
 	if( input->text() == "acpid" )
 		 text = i18n("Advanced Configuration and Power Interface (ACPI) to allow intelligent power management on your system.");
+	else if( input->text() == "apache2" )
+		 text = i18n("Apache v2 is the next generation of the omnipresent Apache web server.");
 	else if( input->text() == "cron" )
 		text = i18n("Cron is a time-based scheduling service.");
 	else if( input->text() == "gpm" )
@@ -287,6 +289,8 @@ void services::showDescription(QListBoxItem* input)
 		text = i18n("klogd is a system daemon which intercepts and logs Linux kernel messages.");
 	else if( input->text() == "rsync" )
 		text = i18n("Rsync is a command line utility traditionally used in synchronizing files between two computers, but rsync can also be used as an effective backup tool. In daemon mode, rsync listens to the default TCP port of 873, serving files in the native rsync protocol. rsync can also be used to synchronize local directories, or via a remote shell such as RSH or SSH.");
+	else if( input->text() == "ssh" )
+		text = i18n("SSH is the portable version of OpenSSH, a free implementation of the Secure Shell protocol as specified by the IETF secsh working group.");
 	else if( input->text() == "sysklog" )
 		text = i18n("Sysklogd provides two system utilities which provide support for system logging and kernel message trapping. Support of both internet and unix domain sockets enables this utility package to support both local and remote logging.");
 	else if( input->text() == "vdr" )
@@ -296,13 +300,15 @@ void services::showDescription(QListBoxItem* input)
 	else if( input->text() == "bluetooth" )
 		text = i18n("Enables Universal interface for wireless communication");
 	else if( input->text() == "cupsys" )
-		text = i18n("The Common UNIX Printing System (or CUPS(tm)) is a printing system. It supports the Internet Printing Protocol (IPP), and has its own filtering driver model for handling various document types.");
+		text = i18n("CUPS (Common UNIX Printing System) is a printing system. It supports the IPP (Internet Printing Protocol), and has its own filtering driver model for handling various document types.");
 	else if( input->text() == "irqbalance" )
 		text = i18n("Irqbalance is a Linux daemon that distributes interrupts over the processors and cores you have in your computer system. The design goal of irqbalance is to do find a balance between power savings and optimal performance.");
 	else if( input->text() == "isdnutils" )
 		text = i18n("Isndutils is only for use with an internal ISDN adapter.");
 	else if( input->text() == "lirc" )
 		text = i18n("LIRC is a package that allows you to decode and send infra-red signals of many (but not all) commonly used remote controls.");
+	else if( input->text() == "mysql" )
+		text = i18n("MySQL is a fast, stable and true multi-user, multi-threaded SQL database server.");
 	else if( input->text() == "virtualbox-ose" )
 		text = i18n("VirtualBox is an application installed on an existing host operating system; within this application, additional operating systems can be loaded and run, each with its own virtual environment.");
 
